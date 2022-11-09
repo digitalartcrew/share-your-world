7120240c815890868d3f5afcc4add7d006f82579
Nicholas <nicholasb@Dontes-MacBook-Pro.local>
2022-11-08 16:18:55 -1000

    landing page (no functionality)

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -6,33 +6,34 @@ import { Button, Image, NavDropdown } from "react-bootstrap";
 const MainNavbar = () => {
   const toggleForm = () => {};
   return (
-    <Navbar style={{ backgroundColor: "lightgray" }}>
+    <Navbar expand="lg" style={{ backgroundColor: "lightgray" }}>
       <Container>
-        <Navbar.Brand href="dashboard">Share Your World</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="dashboard">Dashboard</Nav.Link>
-        </Nav>
+        <Navbar.Brand href="dashboard">Overlay</Navbar.Brand>
+
+        <Nav className="me-auto"></Nav>
         <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
+        <NavDropdown
+          className="text-center dropleft"
+          title={
+            <span>
+              <Image
+                src={"https://avatars.githubusercontent.com/u/10134023?v=4"}
+                alt="UserName profile image"
+                roundedCircle
+                style={{ width: "40px" }}
+              />
+            </span>
+          }
+          id="collapsible-nav-dropdown"
+        >
+          <NavDropdown.Item href="profile">Profile</NavDropdown.Item>
+          <NavDropdown.Item href="settings">Settings</NavDropdown.Item>
+          <NavDropdown.Item href="signout">Sign Out</NavDropdown.Item>
+        </NavDropdown>
+        <Navbar.Collapse className="justify-content-end text-center">
           <Nav>
+            <Nav.Link href="dashboard">Dashboard</Nav.Link>
             <Button style={{ marginRight: "1rem" }}>Create Overlay</Button>
-            <NavDropdown
-              title={
-                <span>
-                  <Image
-                    src={"https://avatars.githubusercontent.com/u/10134023?v=4"}
-                    alt="UserName profile image"
-                    roundedCircle
-                    style={{ width: "40px" }}
-                  />
-                </span>
-              }
-              id="collapsible-nav-dropdown"
-            >
-              <NavDropdown.Item href="profile">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="settings">Settings</NavDropdown.Item>
-              <NavDropdown.Item href="signout">Sign Out</NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- client/src/components/pages/landing.tsx
+++ client/src/components/pages/landing.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import LoginForm from "../LoginForm";
 
 export const LandingPage = () => {
   return (
     <Container>
-      <h1>Landing Page</h1>
+      <Row>
+        <Col className="text-center">
+          <Image
+            src={
+              "https://st4.depositphotos.com/1010613/41685/i/450/depositphotos_416857592-stock-photo-gps-location-search-map-online.jpg"
+            }
+            alt="UserName profile image"
+            style={{ width: "80%", marginTop: "2rem" }}
+          />
+          <h5 style={{ marginTop: "1rem" }}>Share Your World</h5>
+        </Col>
+        <Col>
+          <Container>
+            <LoginForm />
+          </Container>
+        </Col>
+      </Row>
     </Container>
   );
 };
