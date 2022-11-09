436e1119f045962148a753832ad2c8e9fd1fc36a
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-11-08 19:47:08 -1000

    updated models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,15 @@
 const Overlay = require("./overlay-model");
 const User = require("./user-model");
+const Note = require("./note-model");
+const Position = require("./position-model");
+const Plot = require("./plot-model");
+const Tool = require("./tool-model");
 
 module.exports = {
   Overlay,
   User,
+  Note,
+  Position,
+  Plot,
+  Tool,
 };
--- models/overlay-model.js
+++ models/overlay-model.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+const { Note, Position, Plot } = require(".");
 const Schema = mongoose.Schema;
 
 const OverlaySchema = new Schema({
   title: String,
-  location: String,
-  description: String,
+  center: Position,
+  zoom: Number,
+  notes: [Note],
+  plots: [Plot],
 });
 
 module.exports = mongoose.model("Overlay", OverlaySchema);
